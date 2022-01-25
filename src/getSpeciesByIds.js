fefaf0fc9cbbb261424b8426bbf3db4201d162d4
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-01-25 15:34:19 -0300

    Enviando o requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // * seu código aqui
+  const { species } = data;
+  const ident = [...ids];
+
+  return species.filter((specie) => {
+    const { id } = specie;
+    let especie = '';
+
+    for (let i = 0; i < ident.length; i += 1) {
+      if (id === ident[i]) {
+        especie = specie;
+      }
+    }
+    return especie;
+  });
 }
 
 module.exports = getSpeciesByIds;
