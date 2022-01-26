ee3cc308cb3bd49a4ff2e0630514ece8b169804e
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-01-25 21:58:39 -0300

    Enviando o requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const { employees } = data;
+  const name = employeeName;
+
+  let empregado = employees.find((employee) => {
+    const { firstName, lastName } = employee;
+    const fullName = `${firstName} ${lastName}`;
+    let retorno = '';
+
+    if (name === fullName || name === firstName || name === lastName) {
+      retorno = employee;
+    }
+
+    return retorno;
+  });
+
+  if (empregado === undefined) { empregado = {}; }
+
+  return empregado;
 }
+
+console.log(getEmployeeByName());
+
+// Esta função é responsável pela busca das pessoas colaboradoras através do primeiro ou do último nome delas
 
 module.exports = getEmployeeByName;
