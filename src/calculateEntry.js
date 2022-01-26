14601f87ff4796678c2a8ef9cae21fa92cebb036
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-01-26 16:05:44 -0300

    Apangando alguns 'cosole.log()' desnecessarios no codigo do requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -3,11 +3,7 @@ const data = require('../data/zoo_data');
 console.log(data.prices);
 
 function countEntrants(entrants = []) {
-  const entradas = {
-    child: 0,
-    adult: 0,
-    senior: 0,
-  };
+  const entradas = { child: 0, adult: 0, senior: 0 };
 
   if (Object.keys(entrants).length > 0) {
     entradas.child = entrants.filter((entrant) => entrant.age < 18).length;
@@ -28,16 +24,5 @@ function calculateEntry(entrants = []) {
 
   return child + adult + senior;
 }
-
-const entrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-
-console.log(calculateEntry(entrants));
 
 module.exports = { calculateEntry, countEntrants };
