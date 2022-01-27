e8551daee5c6754e3033511ae35b7b6044e44cc9
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-01-27 19:08:11 -0300

    Enviando o requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,32 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function montaRetorno(array) {
+  const residentes = array;
+  const idade = Math.max(...residentes.map((residente) => residente.age));
+
+  const retorno = residentes.find((residente) => residente.age === idade);
+
+  return [retorno.name, retorno.sex, retorno.age];
 }
+
+function getOldestFromFirstSpecies(id) {
+  const { employees } = data;
+  const { species } = data;
+  const animal = employees.filter((employee) => employee.id === id).map((employee) => {
+    const { responsibleFor } = employee;
+    return responsibleFor;
+  })[0][0];
+
+  const residentes = species.filter((specie) => specie.id === animal).map((specie) => {
+    const { residents } = specie;
+    return residents;
+  })[0];
+
+  const infos = montaRetorno(residentes);
+
+  return infos;
+}
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
